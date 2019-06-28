bb9472713e712800260d019a3076fc0378ec0b0a
Ryan <ryan.steinglass@digipen.edu>
2019-06-28 14:32:29 -0700

    added cars, fixed movement bug

---
+++ Frogger/Assets/CarMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        if (transform.position.x >= 9.5 || transform.position.x < -9.5)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+        }
+    }
+}
--- Frogger/Assets/PlayerControl.cs
+++ Frogger/Assets/PlayerControl.cs
@@ -8,37 +8,47 @@ public class PlayerControl : MonoBehaviour
     bool moving = false;
     int move = 10;
     public int moveTime;
+    Vector3 respawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        respawn = transform.position;
     }
     
     // Update is called once per frame
     void Update()
     {
         Vector3 newVel = transform.position;
+        Vector3 newRot = transform.eulerAngles;
 
         if (Input.GetKey("w") && !moving)
         {
             newVel.y += speed;
+            newRot.z = 0;
             moving = true;
+            move = 0;
         }
         else if (Input.GetKey("a") && !moving)
         {
             newVel.x += -speed;
+            newRot.z = 90;
             moving = true;
+            move = 0;
         }
         else if (Input.GetKey("s") && !moving)
         {
             newVel.y += -speed;
+            newRot.z = 180;
             moving = true;
+            move = 0;
         }
         else if (Input.GetKey("d") && !moving)
         {
             newVel.x += speed;
+            newRot.z = 270;
             moving = true;
+            move = 0;
         }
         else if (!(Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")) && moving)
         {
@@ -57,5 +67,15 @@ public class PlayerControl : MonoBehaviour
             }
         }
         transform.position = newVel;
+        transform.eulerAngles = newRot;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.GetComponent<CarMove>())
+        {
+            transform.position = respawn;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
     }
 }
