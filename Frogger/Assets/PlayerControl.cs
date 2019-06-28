c2a76558dd4241687551b9dec79f5a8acb0552a6
Ryan <ryan.steinglass@digipen.edu>
2019-06-28 11:32:23 -0700

    basic movement controls

--- Frogger/Assets/PlayerControl.cs
+++ Frogger/Assets/PlayerControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    int speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,24 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newVel = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            newVel.y += speed;
+        }
+        if (Input.GetKey("a"))
+        {
+            newVel.x += -speed;
+        }
+        if (Input.GetKey("s"))
+        {
+            newVel.y += -speed;
+        }
+        if (Input.GetKey("d"))
+        {
+            newVel.x += speed;
+        }
+        transform.position = newVel;
     }
 }
